36aeac91352981b60867c9c7dcfe2e190d6ad273
cbernjus <christopher@bernjus.de>
2023-06-20 01:49:57 +0200

    feat: add script for downloading a demo bag

---
+++ download_demo_bag.bash
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Check if the file 'demo.bag' already exists
+if [ -f "demo.bag" ]; then
+  echo "The file 'demo.bag' already exists."
+else
+  # Download the demo bag file
+  wget https://storage.googleapis.com/cruise-webviz-public/demo.bag
+fi
